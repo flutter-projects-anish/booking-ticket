59e7a9438ae86bf47e2738f25f179f53f1e5e4da
anish gharti <anishgharti10@gmail.com>
2023-05-25 08:57:55 +0545

    scrollable view

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter_application_1/utils/app_layout.dart';
 import 'package:flutter_application_1/utils/app_styles.dart';
 import 'package:flutter_application_1/widgets/thick_container.dart';
+import 'package:gap/gap.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
@@ -11,12 +12,16 @@ class TicketView extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return SizedBox(
-      width: size.width,
+      width: size.width * 0.85,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+        margin: const EdgeInsets.only(right: 16),
         child: Column(
           children: [
+            /*
+            showing the bluee part of the ticket
+            */
+
             Container(
               decoration: const BoxDecoration(
                   color: Color(0XFF526799),
@@ -24,24 +29,220 @@ class TicketView extends StatelessWidget {
                       topLeft: Radius.circular(21),
                       topRight: Radius.circular(21))),
               padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "NYC",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                          child:
+                              Container()), //expanded and the spacer are working the same way.
+                      const ThickContainer(),
+                      Expanded(
+                        child: Stack(children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context,
+                                  BoxConstraints constraints) {
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: List.generate(
+                                      (constraints.constrainWidth() / 7)
+                                          .floor(),
+                                      (index) => const SizedBox(
+                                            width: 3,
+                                            height: 1,
+                                            child: DecoratedBox(
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white)),
+                                          )),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                              angle: 1.5,
+                              child: const Icon(
+                                Icons.local_airport_rounded,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ),
+
+                      const ThickContainer(),
+                      const Spacer(),
+                      Text(
+                        'Ldn',
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      )
+                    ],
+                  ),
+                  const Gap(5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'New-York',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      Text(
+                        "8H 30M",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'London',
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // showing the orange part of the ticket.
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return Flex(
+                            direction: Axis.horizontal,
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(
+                              (constraints.constrainWidth() / 15).floor(),
+                              (index) => const SizedBox(
+                                width: 5,
+                                height: 1,
+                                child: DecoratedBox(
+                                  decoration:
+                                      BoxDecoration(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // showing the bottom part of thw orange colour
+            Container(
+              decoration: BoxDecoration(
+                color: Styles.orangeColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(21),
+                  bottomRight: Radius.circular(21),
+                ),
+              ),
+              padding: const EdgeInsets.only(
+                  top: 10, left: 16, right: 16, bottom: 16),
               child: Column(children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "NYC",
-                      style:
-                          Styles.headLineStyle3.copyWith(color: Colors.white),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "1 May",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        const Gap(4),
+                        Text(
+                          "date",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        )
+                      ],
                     ),
-                    const Spacer(),
-                    const ThickContainer(),
-                    const Spacer(),
-                    const ThickContainer(),
-                    const Spacer(),
-                    Text(
-                      'London',
-                      style:
-                          Styles.headLineStyle3.copyWith(color: Colors.white),
-                    )
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "08:00 AM",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        const Gap(4),
+                        Text(
+                          "Departure Time",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "23",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        const Gap(4),
+                        Text(
+                          "Number",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        )
+                      ],
+                    ),
                   ],
                 )
               ]),
